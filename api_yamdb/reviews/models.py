2b513c1782ff340c6afa377bf0cf7b3e3801aaa8
IslamAsaliev <asaliev.isl@yandex.ru>
2024-08-06 21:22:02 +0300

    create and registered api apps

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -59,8 +59,8 @@ class GenreTitle(models.Model):
     genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
 
     class Meta:
-        verbose_name = 'жанр произведения'
-        verbose_name_plural = 'Жанры произведений'
+        verbose_name = 'жанр и произведение'
+        verbose_name_plural = 'Жанры и произведения'
 
     def __str__(self):
         return f'Жанр произведения {self.title} - {self.genre}.'
