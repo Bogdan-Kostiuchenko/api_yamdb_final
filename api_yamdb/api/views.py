cd0f2e7d574415c549a18e93103cba04e44c6b19
Светлана Молодцова <molodtsova201@yandex.ru>
2024-08-10 22:39:43 +0300

    правки Светлана

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters, status, viewsets, mixins
 from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework.permissions import (IsAuthenticatedOrReadOnly,
                                         AllowAny,
                                         IsAuthenticated)
