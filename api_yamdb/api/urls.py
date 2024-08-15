a98648f59137959f2ccf33094255416e5bacd854
IslamAsaliev <asaliev.isl@yandex.ru>
2024-08-15 14:49:46 +0300

    urls

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -43,7 +43,11 @@ auth_patterns = [
     path('token/', token_jwt, name='get_token'),
 ]
 
+v1_urlpatterns = [
+    path('', include(v1_router.urls)),
+    path('auth/', include(auth_patterns)),
+]
+
 urlpatterns = [
-    path('v1/', include(v1_router.urls)),
-    path('v1/auth/', include(auth_patterns)),
+    path('v1/', include(v1_urlpatterns)),
 ]
